b9cc27d2bb16fea46ebaef5835682cfc87f875df
Manuel Samudio <manuxscorpiuz12@gmail.com>
2016-06-05 17:17:49 -0500

    Messages with Symetric Encryption

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,31 @@
+class MessagesController < ApplicationController
+
+  before_action :require_user, only: [:index, :new, :create, :show]
+  def index
+    @messages = Message.all
+  end
+  def new
+    @message = Message.new
+  end
+  def create
+   @message = Message.new(message_params)
+   if @message.save
+     redirect_to'/messages'
+   else
+     render 'new'
+   end
+ end
+ def show
+   @message = Message.find(params[:id])
+ end
+ private
+   def message_params
+     salt  = SecureRandom.random_bytes(64)
+     key = ActiveSupport::KeyGenerator.new(params[:message][:secret_key]).generate_key(salt)
+     encrypt = ActiveSupport::MessageEncryptor.new(key)
+     params[:message][:secret_key] = encrypt.encrypt_and_sign(params[:message][:secret_key])
+     params[:message][:content] = encrypt.encrypt_and_sign(params[:message][:content])
+     #decrypt_and_verify(encrypted_data)
+     params.require(:message).permit(:title, :secret_key, :content)
+   end
+end
